71df6cd5b0d7d1be396bb36db87435044e87856c
wessware <stevenshisia@yahoo.com>
2022-11-23 12:27:54 +0300

    data js file

---
+++ data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        id: 1,
+        title: 'About', 
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident numquam minus non.',
+        photo: 'logo.png'
+    },
+    {
+        id: 2,
+        title: 'Executive', 
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident numquam minus non.',
+        photo: 'blog1.jpg'
+    },
+    {
+        id: 3,
+        title: 'News', 
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident numquam minus non.',
+        photo: 'blog2.jpg'
+    },
+    {
+        id: 4,
+        title: 'Blog', 
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident numquam minus non.',
+        photo: 'blog4.jpg'
+    },
+    {
+        id: 5,
+        title: 'Contact', 
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident numquam minus non.',
+        photo: 'patron.jpg' 
+    }
+]
